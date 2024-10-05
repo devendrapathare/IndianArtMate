a3c99ef665b10553eb995cf2f950accbeeb5ec1f
krishm2006 <krish2006mishra@gmail.com>
2024-10-05 21:00:35 +0530

    icons navbar

--- Frontend/src/person-3/Components-3/Nav/Nav.jsx
+++ Frontend/src/person-3/Components-3/Nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = ({ setshowLogin }) => {
 
   const navigate = useNavigate()
 
- const { getTotalCartAmount } = useContext(CartContext)
+  const { getTotalCartAmount } = useContext(CartContext)
   const userId = authUser?._id;
 
   const toggleSidebar = () => {
@@ -26,43 +26,43 @@ const Nav = ({ setshowLogin }) => {
     console.log(`Navigating to ${section}`);
   };
 
-  
+
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
         const response = await fetch(`http://localhost:5000/users/${userId}`);
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.json();
-  
-        const user = data.user; 
+
+        const user = data.user;
         let fullImageUrl;
-  
+
         // console.log("user from nav:", user);
-        
+
         if (user.profilePic.startsWith('http')) {
           fullImageUrl = user.profilePic;
         } else {
           console.log("in else");
           fullImageUrl = `http://localhost:5000/profilePics${user.profilePic.split('/profilePic')[1]}`;
         }
-  
+
         setImage(fullImageUrl);
         // console.log(image);
-        
+
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
     };
-  
+
     const intervalId = setInterval(() => {
       fetchUserProfile();
     }, 600000);
-  
+
     fetchUserProfile();
-  
+
     return () => clearInterval(intervalId);
   }, [userId]);
-  
+
   return (
     <>
       <div className='nav'>
@@ -75,32 +75,45 @@ const Nav = ({ setshowLogin }) => {
           <h2>Indian_ArtMate</h2>
         </div>
         <div className="left common">
-          <Link to='/'><h2>Home</h2></Link>
-
+          <div className="nav-icon">
+            <Link to='/'> <img className='img' src={assets.home_icon} alt="Home" /></Link>
+          </div>
           {!authUser ? (
-            <h2 onClick={() => setshowLogin(true)}>Login</h2>
+            <div className="nav-icon">
+              <img className='img' src={assets.login_icon} alt="Login" onClick={() => setshowLogin(true)} />
+            </div>
+            // <h2 onClick={() => setshowLogin(true)}>Login</h2>
           ) : (
             <>
-              <Link to='/myStore'><h2 onClick={() => onNavClick('store')}>My Store</h2></Link>
+              <div className="nav-icon">
+                <Link to='/myStore'><img className='img' src={assets.store_icon} alt="Store" onClick={() => onNavClick('store')} /></Link>
+              </div>
+              <div className="nav-icon">
+                <Link to='/ProfilePage'><img className='img' src={assets.profile_icon} alt="My Profile" onClick={() => onNavClick('profile')} /></Link>
+              </div>
+              <div className="nav-icon">
+                <Link to='/myChats'><img className='img' src={assets.chat_icon} alt="My Chats" onClick={() => onNavClick('chats')} /></Link>
+              </div>
+              {/* <Link to='/myStore'><h2 onClick={() => onNavClick('store')}>My Store</h2></Link>
               <Link to='/ProfilePage'><h2 onClick={() => onNavClick('profile')}>My Profile</h2></Link>
-              <Link to='/myChats'><h2 onClick={() => onNavClick('chats')}>My Chats</h2></Link>
-              <div className="cart-dot">
-                <Link to='/cart'><h2 onClick={() => onNavClick('cart')}>Cart</h2></Link>
+              <Link to='/myChats'><h2 onClick={() => onNavClick('chats')}>My Chats</h2></Link> */}
+              <div className="cart-dot nav-icon">
+              <Link to='/cart'><img className='img' src={assets.cart_icon} alt="Cart" onClick={() => onNavClick('cart')} /></Link> 
                 <div className={getTotalCartAmount() === 0 ? '' : "dot"}></div>
               </div>
               <div className='navbar-profile'>
                 <img src={image} alt="Profile" />
                 <ul className="nav-profile-dropdown">
-                <li>
-                  <img
-                    src={assets.order_delivery}
-                    alt="myOrders"
-                    onClick={logout}
-                    style={{ cursor: 'pointer' }}
-                  />
-                  <p onClick={()=>navigate('/myOrders')} style={{ cursor: 'pointer' }}>MyOrders</p>
-                </li>
-                <hr />
+                  <li>
+                    <img
+                      src={assets.order_delivery}
+                      alt="myOrders"
+                      onClick={logout}
+                      style={{ cursor: 'pointer' }}
+                    />
+                    <p onClick={() => navigate('/myOrders')} style={{ cursor: 'pointer' }}>MyOrders</p>
+                  </li>
+                  <hr />
                   <li>
                     <img
                       src={assets.logout_icon}
